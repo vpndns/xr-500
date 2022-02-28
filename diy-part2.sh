556472381aa15841d84263c7b3d1b345789467a1
vpndns <71969787+vpndns@users.noreply.github.com>
2022-02-28 11:37:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.253/g' package/base-files/files/bin/config_generate
+
+添加软件包
+git clone https://github.com/project-openwrt/luci-app-koolproxyR package/luci-app-koolproxyR
+
+#更新
+./scripts/feeds update -a
+./scripts/feeds install -a
